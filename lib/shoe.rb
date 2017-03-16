86ad4d380185c4a57ead07df0e526d3a784a8fa6
github username <github email address>
2017-03-16 17:21:02 -0400

    add oo basics with class to github

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,15 @@
+require "pry"
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+    BRANDS.uniq!
   end
 
   def cobble
@@ -11,4 +17,5 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  
 end
